9bdac7497e6017525b4ada57598e799dffb97626
1nayat <inayatw086@gmail.com>
2026-02-16 23:47:28 +0530

    chnges in bootstrap part

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -64,7 +64,7 @@ namespace OnlineShoppingStore.Controllers
 
             if (cartItem != null)
             {
-                cartItem.Quantity++;  
+                cartItem.Quantity++;     
             }
             else
             {
